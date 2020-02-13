9ab625588d073e6866f0ccf9d5f66af0586746f2
Artem <45079018+Stafora@users.noreply.github.com>
2020-02-13 22:11:38 +0600

    Update and rename RedirectUrlComponent.php to RedirectUrl.php

---
+++ RedirectUrl.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Staf
+ * Date: 20.01.2020
+ * Time: 11:17
+ */
+
+namespace stafora\components\RedirectUrl;
+
+
+use Yii;
+use yii\base\Component;
+use stafora\components\RedirectUrl\ParserCSV;
+
+class RedirectUrl extends Component {
+    
+    public $filepath = "@app/components/RedirectUrl/files/redirect.csv";
+
+
+    public function init()
+    {
+        parent::init();
+
+        $parserCSV = new ParserCSV($this->getFilePath());
+        $url = $parserCSV->parseUrl();
+
+        if($url){
+            $this->redirectUrl($url);
+        }
+    }
+
+    public function redirectUrl($url){
+        Yii::$app->getResponse()->redirect($url)->send();
+        exit;
+    }
+
+    protected function getFilePath(): string
+    {
+        $path = Yii::getAlias($this->filepath);
+        return $path;
+    }
+
+}
