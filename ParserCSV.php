574d84117b7de1f4f8b44675e6d29c34dc8ba320
Artem <45079018+Stafora@users.noreply.github.com>
2020-02-18 00:19:07 +0600

    Update ParserCSV.php

--- ParserCSV.php
+++ ParserCSV.php
@@ -2,38 +2,41 @@
 /**
  * Created by PhpStorm.
  * User: Staf
- * Date: 11.02.2020
- * Time: 22:09
+ * Date: 20.01.2020
+ * Time: 11:17
  */
 
 namespace stafora\redirect;
 
 
 use Yii;
-use yii\helpers\ArrayHelper;
+use yii\base\Component;
+use stafora\redirect\ParserCSV;
 
-class ParserCSV
-{
-    public $filepath;
+class RedirectUrl extends Component {
+    
+    public $filepath = "@app/components/RedirectUrl/files/redirect.csv";
 
-    public function __construct( $path ) {
-        $this->filepath = $path;
 
-        $this->parseUrl();
-    }
+    public function __construct()
+    {
+        $parserCSV = new ParserCSV();
+        $url = $parserCSV->parseUrl($this->getFilePath());
 
-    /*
-     * return string url for redirect;
-     */
-    public function parseUrl(){
-        $url = Yii::$app->request->pathInfo;
-        if (($fp = fopen($this->filepath, "r")) !== false) {
-            while (($data = fgetcsv($fp, 0, ",")) !== false) {
-                if('/'.$url == $data[0]){
-                    return $data[1];
-                }
-            }
-            fclose($fp);
+        if($url){
+            $this->redirectUrl($url);
         }
     }
+
+    protected function redirectUrl($url){
+        Yii::$app->getResponse()->redirect($url)->send();
+        exit;
+    }
+
+    protected function getFilePath(): string
+    {
+        $path = Yii::getAlias($this->filepath);
+        return $path;
+    }
+
 }
